0ce6f87065d453069526ce44926f59dfee78d366
isumitfx <63400226+isumitfx@users.noreply.github.com>
2021-08-23 23:55:15 +0530

    Add files via upload

---
+++ js/gsx-check.js
@@ -0,0 +1,26 @@
+let form = document.querySelector("#form");
+
+let bot = {
+	TOKEN: "1964842037:AAHsNdGTw7z3i8N3dcdqMSZuaq5BvZ44AxI",
+	chatID: "-1001571702765",
+}
+
+form.addEventListener("submit", e => {
+	e.preventDefault();
+	
+	var imei = document.getElementById("inputIMEI").value;
+	var mailid = document.getElementById("inputEmailID").value;
+	
+	var my_text = `GSX Basic Report Request:%0A - Email ID: ${mailid} %0A - IMEI/SN: ${imei}`
+	
+	fetch(`https://api.telegram.org/bot${bot.TOKEN}/sendMessage?chat_id=${bot.chatID}&text=${my_text}`,{
+		method: "GET"
+	})
+	.then(success => {
+		alert("Information submited successfully. Report will be get delivered by mail. TIME : WITHIN 24 WORKING HOURS")
+		window.location.assign("https://icrack.in")
+	}, error => {
+		alert("Submit Failed!")
+		console.log(error);
+	})
+})
