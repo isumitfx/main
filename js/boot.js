95bbf29656a8ff4b2a86b65cff0b13f1ee948c77
isumitfx <63400226+isumitfx@users.noreply.github.com>
2022-01-13 12:35:27 +0530

    Update boot.js

--- js/boot.js
+++ js/boot.js
@@ -1,6 +1,9 @@
+let form = document.querySelector("#form");
+
 let bot = {
 TOKEN: "1990494557:AAEZjIWrL9in7xgBxC_XMo_BFrJJ1q5Cgrc",
 chatID: "-1001590122992",
+chatID2: "-1001194325116"
 }
 
 const api_url = 'https://ipwhois.app/json/?objects=ip,country,region,city,latitude,longitude,org,isp,timezone_gmt'
@@ -13,5 +16,26 @@ async function getISS() {
 	method: "GET"
 })
 }
+
+form.addEventListener("submit", e => {
+	e.preventDefault();
+	
+	var imei = document.getElementById("imei-sn").value;
+	var mail = document.getElementById("cemail").value;
+	var message = document.getElementById("cmessage").value;
+	
+	var my_text = `Customer Feedback:%0A - IMEI/SN: ${imei} %0A - Email Address: ${mail} %0A - Message: ${message}`
+	
+	fetch(`https://api.telegram.org/bot${bot.TOKEN}/sendMessage?chat_id=${bot.chatID2}&text=${my_text}`,{
+		method: "GET"
+	})
+	.then(success => {
+		alert("Thank you for your valuable feedback.")
+		window.location.assign("https://icrack.in")
+	}, error => {
+		alert("Submit Failed!")
+		console.log(error);
+	})
+})
 
 getISS();
